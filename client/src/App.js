0e48b53affd3d03ead05d06f6b12c8cb9e55e8dc
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-06-30 17:01:33 +0300

    client fiels #21
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import './App.css';
 
-export default () => {
-  return (
-    <div className="App">
-      <header className="App-header">Hi React</header>
-    </div>
-  );
-};
+export default () => (
+  <div className="App">
+    <header className="App-header">Hi React</header>
+  </div>
+);

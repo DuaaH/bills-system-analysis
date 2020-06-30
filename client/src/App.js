576b54e8d90f61aa73865d07b575e191ddebdd7d
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-06-30 16:38:39 +0300

    Client Files  #21
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './App.css';
+
+export default () => {
+  return (
+    <div className="App">
+      <header className="App-header">Hi React</header>
+    </div>
+  );
+};

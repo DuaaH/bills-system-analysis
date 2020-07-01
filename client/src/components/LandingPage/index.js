0f91350b20493209923a608ad603c34eb26a863b
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-07-01 12:09:15 +0300

    Convert to Folder , add  Landing File

--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import Styles from './Style';
 import { Grid, Box, Typography, Paper } from '@material-ui/core';
 
 export default () => {
+  const classes = Styles();
   return (
     <Box component="div" p={3} width={1}>
       <Grid container item sx={12} justify="center">
-        <Paper elevation={3} style={{ width: '100%' }}>
+        <Paper elevation={3} className={classes.content}>
           <Grid item xs={12}>
             <Typography variant="h6" color="textSecondary" align="center">
               Billbase

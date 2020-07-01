0d3aefd2dbe12b23958336e99ad72196307b863a
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-07-01 11:29:35 +0300

    remove unused code

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import './App.css';
 import Router from './components/Router';
 
-export default () => (
-  <div className="App">
-    <Router />
-  </div>
-);
+export default () => <Router />;

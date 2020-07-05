a601c3c5050a5e68c85b5e79b62da74ad96cb682
Hanan795 <hawawdeh95@gmail.com>
2020-07-05 12:48:21 +0300

    1. create dummyData file for inserting instructions 2.delete inserting instructions from dbBuild.sql 3.build dummyData #39

--- Server/database/dbBuild.js
+++ Server/database/dbBuild.js
@@ -5,8 +5,12 @@ const { readFileSync } = require('fs');
 const connection = require('./connection');
 
 const sql = readFileSync(join(__dirname, 'dbBuild.sql')).toString();
+const dummySql = readFileSync(join(__dirname, 'dummyData.sql')).toString();
 
 connection
   .query(sql)
-  .then(() => console.log('build created successfully!'))
+  .then((res) => {
+    connection.query(dummySql).then((result) => result);
+    console.log("successfully build database")
+  })
   .catch((e) => console.error('failed to build', e.stack));

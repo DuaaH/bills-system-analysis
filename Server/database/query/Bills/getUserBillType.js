29b3d8a516e87f148ca2549bf8476edc6f13b4b5
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-07-06 18:40:42 +0300

    add distinct to query
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- Server/database/query/Bills/getUserBillType.js
+++ Server/database/query/Bills/getUserBillType.js
@@ -2,7 +2,7 @@ const connection = require('../../connection');
 
 module.exports = (userId) => {
   const sql = {
-    text: 'SELECT type FROM bill where users_id = $1',
+    text: 'SELECT DISTINCT  type FROM bill where users_id = $1',
     values: [userId],
   };
   return connection.query(sql.text, sql.values);

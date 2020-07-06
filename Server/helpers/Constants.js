165068bbf26a23921c533c9a536edbb8a78e7d0e
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-07-06 11:00:46 +0300

    Add Helper

---
+++ Server/helpers/Constants.js
@@ -0,0 +1,20 @@
+const ROLE = {
+  ADMIN: 'admin',
+  USER: 'user',
+};
+
+const EVENTSTATUS = {
+  OPEN: 'open',
+  FINISED: 'finised',
+  CANCELED: 'canceled',
+  STARTED: 'started',
+};
+
+const PROVIDERTYPE = {
+  Electricity: 'Electricity',
+  Water: 'Water',
+  Internet: 'Internet',
+  Communication: 'Communication',
+};
+
+module.exports = { ROLE, EVENTSTATUS };

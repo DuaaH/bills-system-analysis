1f606e0bfb739beeb256f68811532071080ce2fa
kholoudfann <141060@ppu.edu.ps>
2020-07-07 14:07:05 +0300

    create path

--- Server/controllers/index.js
+++ Server/controllers/index.js
@@ -1,7 +1,11 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-  res.send('Server is Running');
+console.log('hello');
+const { getPersonalInfo } = require('./User/getPersonalInfo');
+
+router.get('/api/test', (req, res) => {
+  res.send({ hello: 'world' });
 });
+router.get('/api/info', getPersonalInfo);
 
 module.exports = router;

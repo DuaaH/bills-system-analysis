131e69150023c61d1a0589ca0a4e7c85d2904ff9
kholoudfann <141060@ppu.edu.ps>
2020-07-07 14:06:26 +0300

    edit on the query

--- Server/database/query/User/getPersonalinfo.js
+++ Server/database/query/User/getPersonalinfo.js
@@ -3,7 +3,7 @@ const connection = require('../../connection');
 module.exports = (id) => {
   const sql = {
     text:
-      'SELECT display_name , phone ,personal_status ,number_of_individuals , number_of_devices FROM users WHERE number_of_devices >2 ;',
+      ' SELECT users.display_name , users.phone ,users.personal_status ,users.number_of_individuals ,users.number_of_devices, address.town , address.city FROm users INNER JOIN address ON address.id = users.address_id WHERE users.id =1 ;',
     values: [id],
   };
   return connection.query(sql.text, sql.values);

3bd610f73617d52fa1c4d9930b86bec1420e46ae
kholoudfann <141060@ppu.edu.ps>
2020-07-08 12:30:04 +0300

    create query and controller for personal info page

--- Server/controllers/User/getPersonalInfo.js
+++ Server/controllers/User/getPersonalInfo.js
@@ -2,19 +2,21 @@ const {
   successMessage,
   internalErrorMessage,
 } = require('../../helpers/responseMessage');
- const { getPersonalInfo} = require ('../../database/query/User/getPersonalInfo');
- 
+const { getPersonalInfo } = require('../../database/query/User');
 module.exports = (req, res) => {
   const id = 1;
-
   getPersonalInfo(id)
     .then((result) => {
       return res.status(200).json(successMessage(result.rows));
+   
     })
     .catch((err) => {
-      console.log('Error in get  current user', { ...err });
+      console.log('Error in get personal information of current user ', err);
       return res.json(
-        internalErrorMessage(null, 'Error Happens in get  user information '),
+        internalErrorMessage(
+          null,
+          'Error Happens in get User  personal information ',
+        ),
       );
     });
 };
--- Server/database/query/User/getPersonalinfo.js
+++ Server/database/query/User/getPersonalinfo.js
@@ -3,7 +3,7 @@ const connection = require('../../connection');
 module.exports = (id) => {
   const sql = {
     text:
-      ' SELECT users.display_name , users.phone ,users.personal_status ,users.number_of_individuals ,users.number_of_devices, address.town , address.city FROm users INNER JOIN address ON address.id = users.address_id WHERE users.id =1 ;',
+    'SELECT users.display_name , users.phone ,users.personal_status ,users.number_of_individuals ,users.number_of_devices, address.town , address.city FROm users INNER JOIN address ON address.id = users.address_id WHERE users.id =$1;',
     values: [id],
   };
   return connection.query(sql.text, sql.values);

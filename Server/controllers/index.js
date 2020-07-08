65a29e50130f21da8c918fa4f080fb7018f9a711
kholoudfann <141060@ppu.edu.ps>
2020-07-08 12:33:08 +0300

    delete the commit

--- Server/controllers/index.js
+++ Server/controllers/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 
-// console.log('hello');
+
 const { getPersonalInfo } = require('./User');
 
-// router.get('/api/test', (req, res) => {
-//   res.send({ hello: 'world' });
-// });
+
 router.get('/api/info', getPersonalInfo);
 
 module.exports = router;

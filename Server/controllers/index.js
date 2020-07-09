b193d24350fffd0be82808adb9c9f4502c2f4ffb
kholoudfann <141060@ppu.edu.ps>
2020-07-09 12:29:56 +0300

    change the path from api/info to api/profile

--- Server/controllers/index.js
+++ Server/controllers/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 
 const { getPersonalInfo } = require('./User');
 
-router.get('/api/info', getPersonalInfo);
+router.get('/api/profile', getPersonalInfo);
 
 module.exports = router;

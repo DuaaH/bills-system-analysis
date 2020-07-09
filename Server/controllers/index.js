139bdaf00d67a23cc0e43748374286a7d095f9f3
kholoudfann <141060@ppu.edu.ps>
2020-07-09 13:33:06 +0300

    order the code

--- Server/controllers/index.js
+++ Server/controllers/index.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
 
 const { getPersonalInfo } = require('./User');
-
-router.get('/api/profile', getPersonalInfo);
 const { getUserBillType } = require('./Bill');
 
+router.get('/api/profile', getPersonalInfo);
 router.get('/api/home', getUserBillType);
 
 module.exports = router;

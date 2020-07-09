9b8ebd3b4b638761c6d18f1d74c399d8f66a45f9
kholoudfann <141060@ppu.edu.ps>
2020-07-09 12:29:18 +0300

    edit on the status of catch

--- Server/controllers/User/getPersonalInfo.js
+++ Server/controllers/User/getPersonalInfo.js
@@ -7,16 +7,17 @@ module.exports = (req, res) => {
   const id = 1;
   getPersonalInfo(id)
     .then((result) => {
-      return res.status(200).json(successMessage(result.rows));
-   
+      return res.status(200).json(successMessage(result.rows[0]));
     })
     .catch((err) => {
       console.log('Error in get personal information of current user ', err);
-      return res.json(
-        internalErrorMessage(
-          null,
-          'Error Happens in get User  personal information ',
-        ),
-      );
+      return res
+        .status(501)
+        .json(
+          internalErrorMessage(
+            null,
+            'Error Happens in get User  personal information ',
+          ),
+        );
     });
 };

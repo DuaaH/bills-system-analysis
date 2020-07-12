c10002e092befc27c8c2cf2dbe400d26782bc7f9
bananhaj <bananohaj7@gmail.com>
2020-07-12 11:12:44 +0300

    resolving some issues #48

--- Server/controllers/Bill/getLastBill.js
+++ Server/controllers/Bill/getLastBill.js
@@ -7,14 +7,14 @@ const {
 
   module.exports = (req, res) => { 
       const {bill_type } = req.params ;
-      const {limit} = req.body;
-      getLastBill(bill_type, limit)
+      const user_id = 1;
+      getLastBill(bill_type, user_id)
       .then((result) => {
         return res.status(200).json(successMessage(result.rows));
       })
       .catch((err) => {
         console.log('Error in get last bills : ', err );
-        return res.json(
+        return res.status(501).json(
           internalErrorMessage(null, 'Error Happens in get Bill Type '),
         );
       });
--- Server/database/query/Bills/getLastBill.js
+++ Server/database/query/Bills/getLastBill.js
@@ -1,10 +1,13 @@
 const connection =require('../../connection')
 
-module.exports =  (type, limit ) => {
+module.exports =  (type, id ) => {
     const sql = {
-        text: 'SELECT gid, users_id, provider_id , type , total_amount , bill_DATE  , due_DATE , start_DATE  , end_DATE , bill_Number  FROM bill   WHERE type=$1  ORDER BY bill_DATE desc  limit=$2'
-        ,values : [type, limit]
+        text: `SELECT gid, provider_id , type
+         , total_amount , bill_DATE  , due_DATE , start_DATE  , end_DATE , bill_Number 
+          FROM bill 
+          WHERE type=$1 AND users_id=$2 `
+        ,values : [type, id]
     }; 
-    console.log(sql)
+    
     return connection.query(sql.text,sql.values);
 }

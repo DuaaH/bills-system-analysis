728c0485eb59e8ab47ec323c3d76939a36a0f91a
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-07-13 18:27:49 +0300

    Statistics query

--- Server/database/query/Bills/index.js
+++ Server/database/query/Bills/index.js
@@ -1,8 +1,8 @@
-const getLastBill = require('./getLastBill'); 
+const getLastBill = require('./getLastBill');
 const getUserBillType = require('./getUserBillType');
+const getBillStatistics = require('./getBillStatistics');
 
 module.exports = {
   getLastBill,
-  getUserBillType,
+  getBillStatistics,
 };
-

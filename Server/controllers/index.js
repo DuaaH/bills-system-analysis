e800354b6125da5cb13be1721ccb10fea41ab34b
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-07-13 18:25:51 +0300

    add router

--- Server/controllers/index.js
+++ Server/controllers/index.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
-const {  getLastBill, getUserBillType } = require('./Bill');
+const { getLastBill, getUserBillType, getBillStatistics } = require('./Bill');
 const { logIn } = require('./User');
 const { getPersonalInfo } = require('./User');
 
-router.get('/api/bill/:bill_type', getLastBill )
+router.get('/api/bill/:bill_type/statistics/:billId', getBillStatistics);
+router.get('/api/bill/:bill_type', getLastBill);
 router.post('/api/login', logIn);
 router.get('/api/profile', getPersonalInfo);
 router.get('/api/home', getUserBillType);

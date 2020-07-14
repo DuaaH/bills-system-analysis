e0985c6e0c21da48f0fc54fb6a25680fcc6318a5
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-07-14 23:19:37 +0300

    update where
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- Server/database/query/Bills/getBillById.js
+++ Server/database/query/Bills/getBillById.js
@@ -2,7 +2,7 @@ const connection = require('../../connection');
 
 module.exports = (billId) => {
   const sql = {
-    text: 'SELECT * FROM bill where id = $1',
+    text: 'SELECT * FROM bill where gid = $1',
     values: [billId],
   };
   return connection.query(sql.text, sql.values);

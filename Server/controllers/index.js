a24df8468a448994414856ac43041f59d0960b70
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-07-14 23:18:48 +0300

    update /api/bill to /api/bill/getBillByType/
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- Server/controllers/index.js
+++ Server/controllers/index.js
@@ -4,7 +4,7 @@ const { logIn } = require('./User');
 const { getPersonalInfo } = require('./User');
 
 router.get('/api/bill/:bill_type/statistics/:billId', getBillStatistics);
-router.get('/api/bill/:bill_type', getLastBill);
+router.get('/api/bill/getBillByType/:bill_type', getLastBill);
 router.post('/api/login', logIn);
 router.get('/api/profile', getPersonalInfo);
 router.get('/api/home', getUserBillType);

9def38e9764d9d6c4044fd582fd94d8730e07c83
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-07-14 23:19:22 +0300

    statistics controller #70
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- Server/controllers/Bill/getBillStatistics.js
+++ Server/controllers/Bill/getBillStatistics.js
@@ -1,6 +1,7 @@
 const {
   successMessage,
   internalErrorMessage,
+  failedMessage,
 } = require('../../helpers/responseMessage');
 
 const {
@@ -9,25 +10,49 @@ const {
 } = require('../../database/query/Bills');
 
 const { getPersonalInfo } = require('../../database/query/User');
-const { getAddressById } = require('../../database/query/Address');
 
 module.exports = async (req, res) => {
-  const { billId, bill_type } = req.params;
+  const { billId } = req.params;
   const user_id = 1;
 
   try {
+    // get user data  and bill info
     const results = await Promise.all([
       getPersonalInfo(user_id),
       getBillById(billId),
     ]);
 
-    const user = results[0];
-    const bill = results[1];
-    console.log('user', user);
-    console.log('bill', bill);
+    const user = results[0].rows[0];
+    const bill = results[1].rows[0];
 
-    //const address = await getAddressById(user.id);
+    if (!user || !bill) {
+      return res
+        .status(400)
+        .json(
+          failedMessage(null, 'Some Error in getting Bill, please try again'),
+        );
+    }
+    const statistics = (await getBillStatistics(user_id, bill, user)).rows;
+
+    // get atotal_amount from rows object to retrun [.....] with amount
+    const amount = [];
+    const amountLabels = [];
+
+    amount.push(bill.total_amount);
+    amountLabels.push('Me');
+    for (let i = 0; i < statistics.length; i++) {
+      amount.push(statistics[i].total_amount);
+      amountLabels.push(`P-${i + 1}`);
+    }
+
+    console.log('user : ', user);
+    console.log('bill  : ', bill);
+    console.log('amount : ', amount);
+
+    return res.status(200).json(successMessage({ amount, amountLabels }));
   } catch (err) {
+    console.log('Error in get statistics bills : ', err);
+    console.log('Error in get statistics bills : ', err);
     console.log('Error in get statistics bills : ', err);
     return res
       .status(501)

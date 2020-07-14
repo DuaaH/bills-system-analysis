6189d090898c31bc1f99dfc871c39005f6c5b159
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-07-14 20:40:42 +0300

    statistics controller
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- Server/controllers/Bill/getBillStatistics.js
+++ Server/controllers/Bill/getBillStatistics.js
@@ -3,34 +3,72 @@ const {
   internalErrorMessage,
 } = require('../../helpers/responseMessage');
 
-const { getBillStatistics } = require('../../database/query/Bills');
+const {
+  getBillStatistics,
+  getBillById,
+} = require('../../database/query/Bills');
 
-module.exports = (req, res) => {
-  const { billId } = req.params;
+const { getPersonalInfo } = require('../../database/query/User');
+const { getAddressById } = require('../../database/query/Address');
+
+module.exports = async (req, res) => {
+  const { billId, bill_type } = req.params;
   const user_id = 1;
 
-  console.log(req.params);
-  getBillStatistics(billId, user_id)
-    .then((result) => {
-      return res.status(200).json(
-        successMessage({
-          labels: ['ME', 'Person1', 'Person2', 'Person3', 'Person4', 'Person5'],
-          datasets: [
-            {
-              label: 'Consumption',
-              data: [200, 400, 600, 800, 1000, 1500],
+  try {
+    const results = await Promise.all([
+      getPersonalInfo(user_id),
+      getBillById(billId),
+    ]);
 
-              borderColor: '#fff',
-              borderWidth: 1,
-            },
-          ],
-        }),
-      );
-    })
-    .catch((err) => {
-      console.log('Error in get last bills : ', err);
-      return res
-        .status(501)
-        .json(internalErrorMessage(null, 'Error Happens in get Bill Type '));
-    });
+    const user = results[0];
+    const bill = results[1];
+    console.log('user', user);
+    console.log('bill', bill);
+
+    //const address = await getAddressById(user.id);
+  } catch (err) {
+    console.log('Error in get statistics bills : ', err);
+    return res
+      .status(501)
+      .json(internalErrorMessage(null, 'Error Happens in get Bill Type '));
+  }
 };
+
+/**
+ 
+
+
+getBillStatistics(billId, user_id)
+      .then((result) => {
+        return res.status(200).json(
+          successMessage({
+            labels: [
+              'ME',
+              'Person1',
+              'Person2',
+              'Person3',
+              'Person4',
+              'Person5',
+            ],
+            datasets: [
+              {
+                label: 'Consumption',
+                data: [200, 400, 600, 800, 1000, 1500],
+
+                borderColor: '#fff',
+                borderWidth: 1,
+              },
+            ],
+          }),
+        );
+      })
+      .catch((err) => {
+        console.log('Error in get last bills : ', err);
+        return res
+          .status(501)
+          .json(internalErrorMessage(null, 'Error Happens in get Bill Type '));
+      });
+      
+
+ */

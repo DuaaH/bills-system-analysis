6a9b0dda178a68b46593bfd8a0aa1b13d1a7bf58
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-07-15 21:19:53 +0300

    new Bill Router #52
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- Server/controllers/Bill/index.js
+++ Server/controllers/Bill/index.js
@@ -1,9 +1,11 @@
-const getLastBill = require('./getLastBill')
+const getLastBill = require('./getLastBill');
 const getUserBillType = require('./getUserBillType');
+const newBill = require('./newBill');
+const getBillType = require('./getBillType');
 
 module.exports = {
-  getLastBill,  
+  getLastBill,
   getUserBillType,
-}; 
-
-
+  getBillType,
+  newBill,
+};

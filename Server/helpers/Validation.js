e73f42125fb70bb02cbad45bb7349d1c5199aba7
Hanan795 <hawawdeh95@gmail.com>
2020-07-15 18:03:15 +0300

    add validation function for sign up entries #86

--- Server/helpers/Validation.js
+++ Server/helpers/Validation.js
@@ -29,4 +29,27 @@ const validateLoginInfo = (userData) => {
   return schema.validate(userData);
 };
 
-module.exports = { validateEmail, validateLoginInfo };
+const validatesignupInfo = (userData) => {
+  const schema = joi.object({
+    email: joi
+      .string()
+      .min(6)
+      .required()
+      .email(),
+    userName: joi
+      .string()
+      .min(5)
+      .max(30)
+      .required()
+      .pattern(new RegExp('^[a-zA-Z0-9_]')),
+    password: joi
+      .string()
+      .min(8)
+      .required()
+      .pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')),
+    rePassword: joi.ref('password'),
+  });
+  return schema.validate(userData);
+};
+
+module.exports = { validateEmail, validateLoginInfo, validatesignupInfo };

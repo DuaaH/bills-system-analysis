b9903df7091d6474473ba26f87a5e7a7933ac423
yakoobhammouri <jakoub-h2002@hotmail.com>
2020-07-17 00:54:29 +0300

    fix query
    Signed-off-by: yakoobhammouri <jakoub-h2002@hotmail.com>

--- Server/database/query/Bills/newBill.js
+++ Server/database/query/Bills/newBill.js
@@ -3,7 +3,7 @@ const connection = require('../../connection');
 module.exports = (userId, billData) => {
   const sql = {
     text: `INSERT INTO bill (users_id, provider_id,type ,total_amount, bill_DATE, due_DATE, start_DATE,end_DATE , bill_Number) VALUES 
-                     (1 , 1 , 'electricity', 255 , '2020-05-19' , '2020-05-22' , '2020-05-23' , '2020-05-25' , 306)`,
+                     ($1 , $2 , $3, $4 , $5 , $6 , $7 , $8 , $9)`,
     values: [
       userId,
       billData.providerId,
@@ -16,5 +16,6 @@ module.exports = (userId, billData) => {
       billData.billNumber,
     ],
   };
+  console.log(sql);
   return connection.query(sql.text, sql.values);
 };

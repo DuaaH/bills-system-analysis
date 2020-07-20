954536a1f6571a662dd8590db4f68d45371825f7
Hanan795 <hawawdeh95@gmail.com>
2020-07-20 17:01:08 +0300

    use authentication middleware in controller. #95

--- Server/controllers/index.js
+++ Server/controllers/index.js
@@ -3,13 +3,17 @@ const { getLastBill, getUserBillType, getBillStatistics } = require('./Bill');
 const { logIn } = require('./User');
 const { getPersonalInfo } = require('./User');
 const { signUp } = require('./User');
+const authentication = require('../middlewares/authentication');
 
-router.get('/api/bill/:bill_type', getLastBill);
-router.get('/api/bill/:bill_type/statistics/:billId', getBillStatistics);
-router.get('/api/bill/getBillByType/:bill_type', getLastBill);
+router.get(
+  '/api/bill/:bill_type/statistics/:billId',
+  authentication,
+  getBillStatistics,
+);
+router.get('/api/bill/getBillByType/:bill_type', authentication, getLastBill);
 router.post('/api/login', logIn);
-router.get('/api/profile', getPersonalInfo);
-router.get('/api/home', getUserBillType);
+router.get('/api/profile', authentication, getPersonalInfo);
+router.get('/api/home', authentication, getUserBillType);
 router.post('/api/signup', signUp);
 
 module.exports = router;

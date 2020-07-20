119593bfd6235e13fcbcd4420f5592b1a78a46da
bananhaj <bananohaj7@gmail.com>
2020-07-20 16:44:53 +0300

    landing page #22

--- client/src/components/LandingPage/Style.js
+++ client/src/components/LandingPage/Style.js
@@ -1,5 +1,24 @@
 import { makeStyles } from '@material-ui/core/styles';
-import { deepOrange } from '@material-ui/core/colors';
+
 export default makeStyles((theme) => ({
-  content: { width: '100%' },
+  content: { width: '100%', backgroundColor: theme.palette.primary.main },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    flexDirection: 'column ',
+  },
+  button: {
+    margin: theme.spacing(3),
+    width: '80%',
+    ml: theme.spacing(6),
+  },
+  header: {
+    marginTop: theme.spacing(6),
+    marginBottom: theme.spacing(6),
+    marginLeft: theme.spacing(2),
+  },
+  typographyStyle: {
+    marginLeft: theme.spacing(2),
+    marginBottom: theme.spacing(6),
+  },
 }));
--- client/src/components/LandingPage/index.js
+++ client/src/components/LandingPage/index.js
@@ -1,19 +1,67 @@
 import React from 'react';
-import Styles from './Style';
-import { Grid, Box, Typography, Paper } from '@material-ui/core';
+import Style from './Style';
+import {
+  Grid,
+  Box,
+  Typography,
+  Paper,
+  makeStyles,
+  Button,
+  Link,
+} from '@material-ui/core';
 
 export default () => {
-  const classes = Styles();
+  const classes = Style();
   return (
     <Box component="div" p={3} width={1}>
-      <Grid container item sx={12} justify="center">
-        <Paper elevation={3} className={classes.content}>
-          <Grid item xs={12}>
-            <Typography variant="h6" color="textSecondary" align="center">
-              Billbase
-            </Typography>
+      <Grid container item xs={12} justify="center">
+        <Grid item xs={12}>
+          <Typography
+            variant="h4"
+            color="textPrimary"
+            className={classes.header}
+          >
+            Billbase
+          </Typography>
+          <Typography
+            variant="h6"
+            color="textPrimary"
+            className={classes.typographyStyle}
+          >
+            Billbase Do you want to know if you’re spending too much? Have a
+            clear ii of your bills and compare it with other users
+          </Typography>
+
+          <Grid container className={classes.Buttons}>
+            <Grid item xs={12}>
+              <Button
+                variant="contained"
+                color="secondary"
+                className={classes.button}
+              >
+                <Link color="inherit" href="/signup">
+                  Sign up
+                </Link>
+              </Button>
+            </Grid>
+
+            <Grid item xs={12}>
+              <Button
+                variant="contained"
+                color="secondary"
+                className={classes.button}
+              >
+                Sign up with Google
+              </Button>
+            </Grid>
           </Grid>
-        </Paper>
+          <Typography variant="body2" color="textSecondary" align="center">
+            Have an account?
+            <Link color="inherit" href="/login">
+              Log in
+            </Link>
+          </Typography>
+        </Grid>
       </Grid>
     </Box>
   );

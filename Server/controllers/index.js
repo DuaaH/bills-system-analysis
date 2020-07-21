36aa16cf93f67ba1892ba1bdde5276320aae78ec
bananhaj <bananohaj7@gmail.com>
2020-07-21 22:02:22 +0300

    solved conflicts

--- Server/controllers/index.js
+++ Server/controllers/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const {
   getLastBill,
   getUserBillType,
+  getBillStatistics,
   newBill,
   getBillType,
 } = require('./Bill');
@@ -12,30 +13,22 @@ const getAddressByCity = require('./Address/getAddressByCity');
 const getAllAddresses = require('./Address/getAllAdresses');
 const { signUp } = require('./User');
 const authentication = require('../middlewares/authentication');
-const { getProviderById } = require('./Provider');
+const { getProviderById, getProviderByType } = require('./Provider');
 
-<<<<<<< HEAD
-const { getProviderByType } = require('./Provider');
-
-router.get('/api/bill/getBillByType/:bill_type', getLastBill);
-=======
 router.get(
   '/api/bill/:bill_type/statistics/:billId',
   authentication,
   getBillStatistics,
 );
 router.get('/api/bill/getBillByType/:bill_type', authentication, getLastBill);
->>>>>>> master
 router.post('/api/login', logIn);
 router.get('/api/home', authentication, getUserBillType);
 
-<<<<<<< HEAD
 router.get('/api/getBillType', getBillType);
 router.get('/api/providers/getProviders/:bill_type', getProviderByType);
 
 router.post('/api/new-bill', newBill);
 
-=======
 router.get('/api/profile', authentication, getPersonalInfo);
 router.get('/api/home', authentication, getUserBillType);
 router.patch('/api/update', authentication, updateUser);
@@ -47,5 +40,4 @@ router.get(
   authentication,
   getProviderById,
 );
->>>>>>> master
 module.exports = router;

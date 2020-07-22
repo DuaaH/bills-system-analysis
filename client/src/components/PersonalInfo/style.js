7040634dc20a8419551ffeb9baf78d81935e354f
kholoudfann <141060@ppu.edu.ps>
2020-07-22 14:07:19 +0300

    creatr style profile page

--- client/src/components/PersonalInfo/style.js
+++ client/src/components/PersonalInfo/style.js
@@ -2,19 +2,28 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
   image: {
-    margin: theme.spacing(4),
+    margin: theme.spacing(2),
     width: '100px',
     height: '100px',
   },
   Button: {
     backgroundColor: theme.palette.secondary.main,
     marginLeft: '130px',
-    marginTop: theme.spacing(3),
+    marginTop: theme.spacing(2),
+    [theme.breakpoints.down('sm')]: {marginLeft: 90 },
+
   },
   updateButt: {
     width: '100%',
   },
-  gridPosition: {
+  addPosition: {
+    
+    display:'block',
+    textAlign: 'right',
+   
+   
+  },
+  gridPosition: {    
     textAlign: 'right',
   },
   root: {
@@ -23,7 +32,23 @@ export default makeStyles((theme) => ({
   LineHorizantal: {
     width: '100%',
   },
-    
   
+  labelStyle:{
+   marginRight:'15px',
+
+  } ,
+  inputText: {
+    backgroundColor: '#616161',
+    padding: '0 8px',
+    
+    width: '100px',
+    [theme.breakpoints.down('lg')]: { width: 200 },
+    [theme.breakpoints.down('sm')]: { width: 100 },
+  },
+
+  
+ errorTitle: {
+  textAlign: 'right',
+  },
   
 }));

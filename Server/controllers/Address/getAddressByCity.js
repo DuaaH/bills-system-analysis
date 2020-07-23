f226736a385179f2b7c21fda6c6778171f9bdc78
Hanan795 <hawawdeh95@gmail.com>
2020-07-23 16:51:52 +0300

    remove console or clean code

--- Server/controllers/Address/getAddressByCity.js
+++ Server/controllers/Address/getAddressByCity.js
@@ -3,13 +3,11 @@ const {
   internalErrorMessage,
 } = require('../../helpers/responseMessage');
 const { getAddressByCity } = require('../../database/query/Address');
+
 module.exports = (req, res) => {
   const name = req.params;
   getAddressByCity(name)
-    .then((result) => {
-      console.log(result);
-      return res.status(200).json(successMessage(result.rows));
-    })
+    .then((result) => res.status(200).json(successMessage(result.rows)))
     .catch((err) => {
       console.log('Error in get  current city for  current user ', err);
       return res
--- client/src/components/Login/index.js
+++ client/src/components/Login/index.js
@@ -29,8 +29,11 @@ function Login(props) {
   const handleForm = (e) => {
     e.preventDefault();
     const data = { email, password };
-    if (!email || !validateEmail(email)) {
+    if (!email) {
       return setMessage('You must Enter your email first!');
+    }
+    if (!validateEmail(email)) {
+      return setMessage('Make sure that you write your email correctly!');
     }
     if (!password) {
       return setMessage('You must enter passowrd!');
